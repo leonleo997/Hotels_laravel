f43c2dfb657c46e4fd4336b562b78e3ed31efc09
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2018-05-14 00:17:54 -0500

    third commit

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,9 @@ Auth::routes();
 Route::resource('/hoteles','HotelController');
 Route::resource('/comentarios','ComentarioController');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', function(){
+    return redirect()->route('hoteles.index');
+});
 
 Route::get('/welcome', function(){
     return view('welcome');
